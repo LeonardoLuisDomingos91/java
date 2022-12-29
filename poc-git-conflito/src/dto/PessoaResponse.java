1833be48b4134c469bbf2eaad2da9be5647bb048
LeonardoLuisDomingos91 <l_ldomingos@hotmail.com>
2022-12-29 15:34:14 -0300

    comitando valores para nome e cpf

--- poc-git-conflito/src/dto/PessoaResponse.java
+++ poc-git-conflito/src/dto/PessoaResponse.java
@@ -2,7 +2,7 @@ package dto;
 
 public class PessoaResponse {
 
-    private String nome;
+    private String nome = "Leonardo";
 
-    private String cpf;
+    private String cpf = "400.236.8688-80";
 }
