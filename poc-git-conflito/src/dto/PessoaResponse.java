98a307fa8f25291a4d49b1c36fd196e93a684b7e
LeonardoLuisDomingos91 <l_ldomingos@hotmail.com>
2022-12-29 15:39:12 -0300

    alterando nome e cpf

--- poc-git-conflito/src/dto/PessoaResponse.java
+++ poc-git-conflito/src/dto/PessoaResponse.java
@@ -2,7 +2,7 @@ package dto;
 
 public class PessoaResponse {
 
-    private String nome = "Leonardo";
+    private String nome = "Luis";
 
-    private String cpf = "400.236.8688-80";
+    private String cpf = "511.347.979-90";
 }
